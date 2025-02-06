bcf2d72d70ad679dcc0c0d4746c2a3d8992cfd15
dvori etinger <dvora215088@gmail.com>
2025-02-06 20:40:05 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,88 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using TodoApi;
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthorization(); 
+
+builder.Services.AddControllers(); 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin() 
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                      });
+});
+
+// הוספת Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// הוספת DbContext
+builder.Services.AddDbContext<ToDoDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("ToDoDB"), new MySqlServerVersion(new Version(8, 0, 41))));
+
+var app = builder.Build();
+
+// הפעלת CORS
+app.UseCors(MyAllowSpecificOrigins);
+
+// הפעלת Swagger
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty; 
+});
+
+// הפעלת Authorization
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("/items", async (ToDoDbContext db) =>
+{
+    return await db.Items.ToListAsync();
+});
+
+app.MapGet("/items/{id}", async (int id, ToDoDbContext db) =>
+{
+    return await db.Items.FindAsync(id) is Item item ? Results.Ok(item) : Results.NotFound();
+});
+
+app.MapPost("/items/post", async (Item item, ToDoDbContext db) =>
+{
+    db.Items.Add(item);
+    await db.SaveChangesAsync();
+    return Results.Created($"/items/{item.Id}", item);
+});
+
+app.MapPut("/items/{id}", async (int id, Item updatedItem, ToDoDbContext db) =>
+{
+    var item = await db.Items.FindAsync(id);
+    if (item is null) return Results.NotFound();
+
+    item.Name = updatedItem.Name;
+    item.IsComplete = updatedItem.IsComplete;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/items/{id}", async (int id, ToDoDbContext db) =>
+{
+    var item = await db.Items.FindAsync(id);
+    if (item is null) return Results.NotFound();
+
+    db.Items.Remove(item);
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.Run();
